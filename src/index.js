b700d711c3e752c57e07d935501577d252454649
Steve Morning <ocioej@gmail.com>
2022-05-29 15:27:03 -0300

    Manejo de errores Promise catch

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles.css';
 const heroeId1 = 'capi';
 const heroeId2 = 'iron';
 
-// Codigo en base a Callbacks
+// ############################# Codigo en base a Callbacks #############################
 // Al tener varias variables se vuelve un problema con manejar los CALLBACK  
 // por que se llama una funcion dentro otra
 //buscarHeroeCallback();
@@ -26,7 +26,7 @@ buscarheroe(heroeId1, (err, heroe1) => {
 */
 
 
-
+//##########################################################
 //Las promesas se manejan con THEN - CATCH - FINALLY
 /*
 buscarHeroe(heroeId1).then(heroe1 => {
@@ -37,22 +37,24 @@ buscarHeroe(heroeId1).then(heroe1 => {
 });
 */
 
-
+//##########################################################
 // Para manejar todas la promesas se utiliza Promise.all
-Promise.all([buscarHeroe(heroeId1),buscarHeroe(heroeId2)]).then(heroes => {
+/* 
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)]).then(heroes => {
     console.log('promise.all', heroes);
     console.log(`Enviando a ${heroes[0].nombre} y a ${heroes[1].nombre} a la mision`);
 })
-
+ */
 // Para desesctructurar un arreglo   then(heroes =>       then(([heroe1,heroe2]) => 
-Promise.all([buscarHeroe(heroeId1),buscarHeroe(heroeId2)]).
-then(([heroe1,heroe2]) => {
-    //console.log('promise.all', heroes);
-    console.log(`Enviando a ${heroe1.nombre} y a ${heroe2.nombre} a la mision`);
-})
-
-
-
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)]).
+    then(([heroe1, heroe2]) => {
+        //console.log('promise.all', heroes);
+        console.log(`Enviando a ${heroe1.nombre} y a ${heroe2.nombre} a la mision`);
+    }).catch(err => {
+        alert(err);
+    }).finally(()=> {
+        console.log ('Se termino el promise.all');
+    })
 
 
 console.log('Fin del Programa');
